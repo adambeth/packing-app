ced72dc2159c6da3eb901ec5ec6b31bae50eb052
Adam Bethlehem <30418746+adambeth@users.noreply.github.com>
2024-05-13 07:28:00 +0200

    Stats added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,141 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: true },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+// ];
 
 function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItems(id) {
+    setItems(items.filter((item) => item.id !== id));
+  }
+
+  function handlePacked(id) {
+    setItems(
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form onHandleAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItems}
+        onPacked={handlePacked}
+      />
+      <Stats items={items} />
     </div>
   );
+}
+
+function Form({ onHandleAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+
+    const newItem = {
+      id: Date.now(),
+      description,
+      quantity,
+      packed: false,
+    };
+    onHandleAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+  return (
+    <>
+      <form className="add-form" onSubmit={handleSubmit}>
+        <h3>What do you need for your 😍 trip?</h3>
+        <select
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value))}
+        >
+          {Array.from({ length: 20 }, (_, i) => i + 1).map((v) => (
+            <option key={v} value={v}>
+              {v}
+            </option>
+          ))}
+        </select>
+        <input
+          placeholder="Items.."
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+
+        <button>Add</button>
+      </form>
+    </>
+  );
+}
+
+function PackingList({ items, onDeleteItem, onPacked }) {
+  return (
+    <div className="list">
+      <ul>
+        {items.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+            onDeleteItem={onDeleteItem}
+            onPacked={onPacked}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item, onDeleteItem, onPacked }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onPacked(item.id)}
+      ></input>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
+    </li>
+  );
+}
+function Stats({ items }) {
+  let numItems = items.length;
+  let numPacked = items.filter((item) => item.packed).length;
+  let percentagePacked = Math.round((numPacked / numItems) * 100);
+  return (
+    <>
+      <footer className="stats">
+        <em>
+          {percentagePacked === 100
+            ? ` You have everything you need, LET'S GO  ✈️ `
+            : items.length > 0
+            ? `You have ${numItems} on your list and you have already packed ${percentagePacked}%`
+            : `Start Packing `}
+        </em>
+      </footer>
+    </>
+  );
+}
+
+function Logo() {
+  return <h1>🏝️ Far Away Travels 🧳</h1>;
 }
 
 export default App;
